0171c76c04966248b5f0cdaeabfc4ad585b7c511
te68 <te68@cornell.edu>
2019-07-15 17:57:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here:
+function driversWithRevenueOver(array, revenue){
+  return array.filter(function(person){
+    return person.revenue > revenue
+  })
+}
+
+function driverNamesWithRevenueOver(array, revenue) {
+  let newArray = driversWithRevenueOver(array, revenue)
+  return newArray.map(function(person) {
+    return person.name
+  })
+}
+
+function exactMatch(array, object) {
+  return array.filter(function(person){
+    return person[Object.keys(object)[0]] === object[Object.keys(object)[0]]
+  })
+}
+
+function exactMatchToList(array, object) {
+  let newArray = exactMatch(array, object)
+  return newArray.map(function(person) {
+    return person.name
+  })
+}
